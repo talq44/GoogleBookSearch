61e6e3a17e5262036d6671dcd22dafc057e50f62
changkyu <changkyu@bejewel.kr>
2024-12-07 14:44:01 +0900

    [#3] repository 개선

--- Projects/Domain/SearchListDomain/Sources/SearchListRepositoryImpl.swift
+++ Projects/Domain/SearchListDomain/Sources/SearchListRepositoryImpl.swift
@@ -16,7 +16,44 @@ final class SearchListRepositoryImpl: SearchListRepository {
     func fetch(
         _ request: any SearchListRequest
     ) async -> Result<any SearchListResponse, SearchListError> {
+        let request = GetBooksRequestDTO(
+            q: request.query,
+            startIndex: request.page,
+            maxResults: request.perPage
+        )
         
-        return .failure(.unKnown)
+        do {
+            let response = try await self.remoteDatasource.get_books(
+                request: request
+            )
+            
+            return .success(self.toDomain(response))
+        } catch {
+            guard let apiError = error as? UserAPICoreError else {
+                return .failure(.unKnown)
+            }
+            return .failure(.restAPI(statusCode: apiError.statusCode))
+        }
+    }
+    
+    private func toDomain(
+        _ response: GoogleBooksResponse
+    ) -> SearchListResponse {
+        return SearchListResponseImpl(
+            totalCount: response.totalItems,
+            items: (response.items ?? []).map {
+                let tunmbnail = $0.imageLinks?.thumbnail ??
+                $0.imageLinks?.smallThumbnail ??
+                ""
+                
+                return SearchListItemImpl(
+                    id: $0.id,
+                    title: $0.volumeInfo.title,
+                    thumbnail: tunmbnail,
+                    authors: ($0.volumeInfo.authors ?? [])
+                        .joined(separator: ",")
+                )
+            }
+        )
     }
 }
